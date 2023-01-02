40d77ccb2776456a213675f2d9846d27e889b9c1
Anindra <anindrakarmakar@proton.me>
2023-01-02 14:23:57 +0530

    Init Commit

---
+++ src/main/java/com/codeavenge/evmadhyam/EvMadhyamApplication.java
@@ -0,0 +1,13 @@
+package com.codeavenge.evmadhyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvMadhyamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvMadhyamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeavenge/evmadhyam/EvMadhyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeavenge.evmadhyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvMadhyamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
