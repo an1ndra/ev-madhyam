8fe93d0770e7242da785d25688a9575c3ca89149
Anindra <anindrakarmakar@protonmail.com>
2023-02-22 11:16:49 +0530

    remove unused file

--- src/main/java/com/codeavenge/evmadhyam/Repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeavenge.evmadhyam.Repository;
-
-import com.codeavenge.evmadhyam.Model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
